64a65fa38757372dd1e3a761d67362107758356f
Andrey <megagran.work@gmail.com>
2023-08-29 17:05:33 +0600

    Исправлены классы для логов. Добавлена логика получения и сохранения логов в хранилище логов.

--- src/models/logs.py
+++ src/models/logs.py
@@ -4,16 +4,24 @@ from decimal import Decimal
 
 
 @dataclass
-class BalanceLog:
-    before: Decimal
-    after: Decimal
-    changes: Decimal
-
-
-@dataclass
 class CommonLog:
     card_number: str
     before: Decimal
     after: Decimal
     changes: Decimal
     _datetime_utc: datetime
+
+    @property
+    def datetime_utc(self):
+        return self._datetime_utc
+
+    @datetime_utc.setter
+    def datetime_utc(self, value: datetime):
+        self._datetime_utc = value
+
+
+@dataclass
+class BalanceLog(CommonLog):
+    before: Decimal
+    after: Decimal
+    changes: Decimal
--- src/repositories/log_storage.py
+++ src/repositories/log_storage.py
@@ -1,3 +1,4 @@
+from bisect import bisect_left, bisect_right
 from collections import defaultdict
 from datetime import datetime
 
@@ -6,14 +7,27 @@ from src.models.logs import BalanceLog, CommonLog
 
 class LogStorage:
     def __init__(self):
-        self.balance_logs: defaultdict[str, list[BalanceLog]] = defaultdict(list)
-        self.other_logs: list[CommonLog] = []
+        self._balance_logs: defaultdict[str, list[BalanceLog]] = defaultdict(list)
+        self._other_logs: list[CommonLog] = []
 
-    def save_log(self, card_number: str, log: BalanceLog | CommonLog) -> None:
+    def save(self, log: CommonLog):
         if isinstance(log, BalanceLog):
-            self.balance_logs[card_number].append(log)
+            if log.card_number in self._balance_logs:
+                self._balance_logs[log.card_number].append(log)
+            else:
+                self._balance_logs[log.card_number] = [log]
         else:
-            self.other_logs.append(log)
+            self._other_logs.append(log)
 
-    def get_balance_history(self, card_number: str, from_datetime: datetime, to_datetime: datetime) -> list[BalanceLog]:
-        return [log for log in self.balance_logs.get(card_number, []) if from_datetime <= log._datetime_utc <= to_datetime]
+    def get_balance_history(self, card_number: str, from_date: datetime, to_date: datetime) -> list[BalanceLog]:
+        logs = self._balance_logs.get(card_number, [])
+
+        if logs:
+            start_date = bisect_left(logs, from_date)
+            end_date = bisect_right(logs, to_date)
+
+            return logs[start_date:end_date]
+        return []
+
+    def _get_datetime_from_log(self, log: BalanceLog):
+        return log.datetime_utc
