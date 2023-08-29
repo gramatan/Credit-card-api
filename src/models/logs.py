8d61b91a872008ef84e8cd93d6b3541ae2e85ada
Andrey <megagran.work@gmail.com>
2023-08-29 17:29:22 +0600

    Buity for linters without logic change.

--- src/models/logs.py
+++ src/models/logs.py
@@ -1,3 +1,4 @@
+"""Модуль содержит классы для логирования изменений по карте."""
 from dataclasses import dataclass
 from datetime import datetime
 from decimal import Decimal
@@ -5,6 +6,8 @@ from decimal import Decimal
 
 @dataclass
 class CommonLog:
+    """Базовый класс для логирования."""
+
     card_number: str
     before: Decimal
     after: Decimal
@@ -13,15 +16,27 @@ class CommonLog:
 
     @property
     def datetime_utc(self):
+        """Геттер для времени в UTC.
+
+        Returns:
+            datetime: Время в UTC.
+        """
         return self._datetime_utc
 
     @datetime_utc.setter
-    def datetime_utc(self, value: datetime):
-        self._datetime_utc = value
+    def datetime_utc(self, new_datetime: datetime):
+        """Сеттер для времени в UTC.
+
+        Args:
+            new_datetime (datetime): Новое время в UTC.
+        """
+        self._datetime_utc = new_datetime
 
 
 @dataclass
 class BalanceLog(CommonLog):
+    """Класс для логирования изменений баланса."""
+
     before: Decimal
     after: Decimal
     changes: Decimal
