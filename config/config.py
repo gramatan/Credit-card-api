289d1913c74ef9defb0ea4219e40287797b394ad
Andrey <megagran.work@gmail.com>
2023-08-30 13:00:35 +0600

    User and UserStorage updated. moved some text messages to config

--- config/config.py
+++ config/config.py
@@ -3,3 +3,9 @@
 SECRET_KEY = '27946a0b61bb4f8b3d7613c012e8badb0fd28d6fb79f2286de87c3bed42d143b'  # noqa: S105, E501
 ALGORITHM = 'HS256'
 TOKEN_TTL = 30
+
+USER_EXISTS_ERROR = 'Пользователь с таким номером карты уже существует'
+USER_NOT_FOUND_ERROR = 'Пользователь с таким номером карты не найден'
+USER_LIMIT_ERROR = 'Нельзя установить лимит меньше нуля'
+USER_BALANCE_ERROR = 'Нельзя установить баланс меньше нуля'
+USER_BALANCE_LIMIT_ERROR = 'Нельзя установить баланс меньше лимита'
--- src/repositories/user_storage.py
+++ src/repositories/user_storage.py
@@ -1,46 +1,115 @@
+"""Репозиторий для храненилища пользователей."""
 from decimal import Decimal
 
+from config.config import USER_EXISTS_ERROR, USER_NOT_FOUND_ERROR
 from src.models.user import User
 
 
 class UserStorage:
-    def __init__(self):
-        self.active: dict[str, User] = {}
-        self.closed: list[User] = []
+    """Класс для хранения пользователей."""
 
-    def add(self, card_number: str, info: dict) -> None:
+    def __init__(self):
+        """Инициализация репозитория."""
+        self.active: dict[str, User] = {}   # type: ignore
+        self.closed: list[User] = []        # type: ignore
+
+    def add(self, card_number: str, user_info: dict) -> None:
+        """
+        Добавление пользователя.
+
+        Args:
+            card_number (str): Номер карты.
+            user_info (dict): Информация о пользователе.
+
+        Raises:
+            ValueError: Если пользователь с такой картой уже существует.
+        """
         if self._is_exist_user(card_number, in_active=True, in_closed=True):
-            raise ValueError(f'Пользователь с картой {card_number} уже существует')
-        user = User(card_number=card_number, limit=Decimal(0), info=info)
+            raise ValueError(USER_EXISTS_ERROR)
+        user = User(
+            card_number=card_number,
+            limit=Decimal(0),
+            info=user_info,
+            _balance=Decimal(0),
+        )
         self.active[card_number] = user
 
     def get_user(self, card_number: str) -> User | None:
-        user = self.active.get(card_number)
-        if user:
-            return user
+        """
+        Получение пользователя по номеру карты.
 
-        for user in self.closed:
-            if user.card_number == card_number:
-                user = user
-                break
-        return user
+        Args:
+            card_number (str): Номер карты.
+
+        Returns:
+            User | None: Пользователь.
+        """
+        return self.active.get(card_number)
 
     def update_user(self, user: User) -> User:
-        if not self._is_exist_user(user.card_number, in_active=True, in_closed=True):
-            raise ValueError(f'Пользователя с картой {user.card_number} не существует')
+        """
+        Обновление пользователя.
+
+        Args:
+            user (User): Пользователь.
+
+        Returns:
+            User: Пользователь.
+
+        Raises:
+            ValueError: Если пользователь не существует.
+        """
+        if not self._is_exist_user(     # noqa: WPS337
+            user.card_number,
+            in_active=True,
+            in_closed=False,
+        ):
+            raise ValueError(USER_NOT_FOUND_ERROR)
         self.active[user.card_number] = user
         return user
 
     def close(self, card_number: str) -> bool:
-        if not self._is_exist_user(card_number, in_active=True, in_closed=False):
-            raise ValueError(f'Пользователь с картой {card_number} не активен или не существует')
-        user = self.active.pop(card_number)
-        self.closed.append(user)
-        return user.card_number == self.closed[-1].card_number and user not in self.active
+        """
+        Закрытие пользователя.
 
-    def _is_exist_user(self, card_number: str, in_active: bool = True, in_closed: bool = True) -> bool:
-        if in_active and card_number in self.active:
-            return True
-        if in_closed and any(user for user in self.closed if user.card_number == card_number):
-            return True
-        return False
+        Args:
+            card_number (str): Номер карты.
+
+        Returns:
+            bool: True, если пользователь закрыт.
+
+        Raises:
+            ValueError: Если пользователь не существует или не активен.
+        """
+        if not self._is_exist_user(     # noqa: WPS337
+            card_number,
+            in_active=True,
+            in_closed=False,
+        ):
+            raise ValueError(USER_NOT_FOUND_ERROR)
+        self.closed.append(self.active.pop(card_number))
+        return card_number not in self.active
+
+    def _is_exist_user(
+        self,
+        card_number: str,
+        in_active: bool = True,
+        in_closed: bool = True,
+    ) -> bool:
+        """
+        Проверка существования пользователя.
+
+        Args:
+            card_number (str): Номер карты.
+            in_active (bool): Проверять в активных.
+            in_closed (bool): Проверять в закрытых.
+
+        Returns:
+            bool: True, если пользователь существует.
+        """
+        in_active_condition = in_active and card_number in self.active
+        in_closed_condition = in_closed and any(
+            user for user in self.closed if user.card_number == card_number
+        )
+
+        return in_active_condition or in_closed_condition
