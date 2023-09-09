e2efc34493fa4ab0e71d23955d515efae9e04a81
Andrey <megagran.work@gmail.com>
2023-09-09 22:09:18 +0600

    Убраны упоминания эндпоинта verify для хорошего мердж реквеста.

--- src/services/transactions_service.py
+++ src/services/transactions_service.py
@@ -1,19 +1,11 @@
 """Сервис для транзакций."""
-import asyncio
 from decimal import Decimal
 
-from deepface import DeepFace
-from fastapi import File, UploadFile
-
-from config.config import UNVERIFIED_BALANCE, VERIFIED_BALANCE
 from src.repositories.log_storage import LogStorage
 from src.repositories.token_repository import TokenRepository
 from src.repositories.transactions import Transactions
 from src.repositories.user_storage import UserStorage
-from src.schemas.transactions_schemas import (
-    TransactionRequest,
-    VerificationRequest,
-)
+from src.schemas.transactions_schemas import TransactionRequest
 from src.services.handler_utils import raise_unauthorized_exception
 
 
@@ -88,62 +80,3 @@ class TransactionsService:
             card_number=card_number,
             balance=new_balance,
         )
-
-    async def verify(   # noqa: WPS210
-        self,
-        card_number: str,
-        token: str,
-        selfie: UploadFile = File(...),
-        document: UploadFile = File(...),
-    ) -> VerificationRequest:
-        """
-        Сервис для верификации пользователя.
-
-        Args:
-            card_number (str): Номер карты.
-            token (str): Токен.
-            selfie (UploadFile): Селфи пользователя.
-            document (UploadFile): Документ пользователя.
-
-        Returns:
-            VerificationRequest: Результат верификации.
-        """
-        if not self.token_repo.verify_token(token):
-            raise_unauthorized_exception()
-
-        verification_result = await self._verify(card_number, selfie, document)
-
-        if verification_result:
-            new_limit = Decimal(VERIFIED_BALANCE)
-            verification_response = VerificationRequest(verified=True)
-        else:
-            new_limit = Decimal(UNVERIFIED_BALANCE)
-            verification_response = VerificationRequest(verified=False)
-
-        self.transactions.change_limit(
-            card_number=card_number,
-            new_limit=new_limit,
-        )
-
-        return verification_response
-
-    async def _verify(self, card_number, selfie, document):  # noqa: WPS210
-        from main import executor  # noqa: WPS433
-
-        selfie_path = f'{card_number}_selfie_tmp.jpg'
-        document_path = f'{card_number}_document_tmp.jpg'
-
-        with open(selfie_path, 'wb') as selfie_buffer:
-            selfie_buffer.write(selfie.file.read())
-
-        with open(document_path, 'wb') as doc_buffer:
-            doc_buffer.write(document.file.read())
-
-        loop = asyncio.get_running_loop()
-        verification_result = await loop.run_in_executor(
-            executor,
-            DeepFace.verify,
-            selfie_path,
-            document_path,
-        )
-        return verification_result['verified']
