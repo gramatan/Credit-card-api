d4323c6a7880e2ead6738bbc060229ab7a4f4ef6
Andrey <megagran.work@gmail.com>
2023-09-16 18:52:52 +0600

    Убрал неактуальные файлы

--- credit_card_verify/src/schemas/verify_schemas.py
+++
@@ -1,8 +0,0 @@
-"""Схема для ответа по верификации изображений."""
-from pydantic import BaseModel
-
-
-class VerificationResponse(BaseModel):
-    """Схема для результата верификации."""
-
-    verified: bool
