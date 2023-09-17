16f6c1052b1487aebcffc719f34d9c5b0d782da7
Andrey <megagran.work@gmail.com>
2023-09-17 13:36:44 +0600

    Исправил файл со скриптом для прохождения линтеров.

--- get_api.py
+++ get_api.py
@@ -1,14 +1,17 @@
-import yaml
+"""Скрипт для генерации документации по API."""
 import os
 
+import yaml  # type: ignore
+
+from main_auth import app as auth_app
+from main_balance import app as balance_app
+
 if __name__ == '__main__':
-    if not os.path.exists("docs"):
-        os.mkdir("docs")
+    if not os.path.exists('docs'):
+        os.mkdir('docs')
 
-    from main_auth import app
-    with open("docs/cc_auth.yaml", "w", encoding="utf-8") as f:
-        yaml.dump(app.openapi(), f, allow_unicode=True)
+    with open('docs/cc_auth.yaml', 'w', encoding='utf-8') as auth_api_file:
+        yaml.dump(auth_app.openapi(), auth_api_file, allow_unicode=True)
 
-    from main_balance import app
-    with open("docs/cc_balance.yaml", "w", encoding="utf-8") as f:
-        yaml.dump(app.openapi(), f, allow_unicode=True)
+    with open('docs/cc_balance.yaml', 'w', encoding='utf-8') as b_api_file:
+        yaml.dump(balance_app.openapi(), b_api_file, allow_unicode=True)
