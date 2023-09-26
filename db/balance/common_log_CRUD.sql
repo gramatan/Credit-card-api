49c727abb220d6e2b9022adf20dcb29664acd190
Andrey <megagran.work@gmail.com>
2023-09-26 15:36:33 +0600

    Добавлены нетипичные скрипты(не должны использоваться на бою). Обновлен readme.

--- db/balance/common_log_CRUD.sql
+++ db/balance/common_log_CRUD.sql
@@ -8,5 +8,14 @@ SELECT * FROM common_logs
 WHERE card_number_id = 1 AND
         datetime_utc BETWEEN '1970-01-01 00:00:00' AND '2970-01-01 00:00:00';
 
+-- читаем все логи(нетипичное использование)
+select * from common_logs;
+
+-- обвноление логов(нетипичное использование)
+UPDATE balance_logs
+SET balance_before = 100000, balance_after = 200000, changes = 100000
+WHERE card_number_id = 4 AND
+        datetime_utc = '2023-09-26 09:16:00.625240';
+
 -- удаляем логи по номеру карточки
 DELETE FROM common_logs WHERE card_number_id = 1;
