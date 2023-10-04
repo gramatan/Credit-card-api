a4bf8857b22edf6f4327567ed354e2595120f442
Andrey <megagran.work@gmail.com>
2023-10-04 12:47:53 +0600

    lets try gitlab-ci.

--- config/config.py
+++ config/config.py
@@ -28,7 +28,8 @@ VERIFICATION_HOST = 'cc_verify'   # NOQA: S104
 KAFKA_HOST = 'cc_kafka'
 
 # POSTGRES_HOST = 'localhost'         # NOQA: E800
-POSTGRES_HOST = 'cc_postgres'     # NOQA: E800
+# POSTGRES_HOST = 'cc_postgres'     # NOQA: E800
+POSTGRES_HOST = 'postgresql'     # NOQA: E800
 
 
 PATH_PREFIX = '/api'
