d10ea720261891367a747fa1169aa34bc191dc04
Andrey <megagran.work@gmail.com>
2023-10-06 14:36:46 +0600

    -

--- create_db.py
+++ create_db.py
@@ -1,4 +1,5 @@
-from sqlalchemy import create_engine
+import asyncpg
+import asyncio
 
 from config.config import (
     POSTGRES_DB_NAME,
@@ -8,9 +9,18 @@ from config.config import (
     POSTGRES_PORT,
 )
 
-postgres_table_uri = f'postgresql://{POSTGRES_DB_USER}:{POSTGRES_DB_PASS}@{POSTGRES_HOST}:{POSTGRES_PORT}/postgres'
 
-engine = create_engine(postgres_table_uri, echo=True)
-connection = engine.connect()
-connection.execute(f'CREATE DATABASE {POSTGRES_DB_NAME}')
-connection.close()
+async def create_database():
+    conn = await asyncpg.connect(
+        user=POSTGRES_DB_USER,
+        password=POSTGRES_DB_PASS,
+        database='postgres',
+        host=POSTGRES_HOST,
+        port=POSTGRES_PORT,
+    )
+    try:
+        await conn.execute(f'CREATE DATABASE {POSTGRES_DB_NAME}')
+    finally:
+        await conn.close()
+
+asyncio.run(create_database())
